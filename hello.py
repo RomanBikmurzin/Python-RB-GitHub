6fc6f02683b26cade61eb7d98e6464c801c4964a
RomanBikmurzin <114301851+RomanBikmurzin@users.noreply.github.com>
2025-07-15 10:04:22 +0400

    update hello world to check GitHub Action 4-steps pipeline

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
-print('hello world')
+import pytest
+
+
+def say_hello():
+    greeting_phrase = "hello world"
+    return greeting_phrase
+
+
+def test_say_hello():
+    actual_result = say_hello()
+    assert actual_result == expected_result
